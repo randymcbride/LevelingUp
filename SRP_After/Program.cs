cf232aa0eb61a2975df9d357c763012425b6841e
Randy McBride <randy.mcbride@centrallogic.com>
2019-05-16 02:38:17 +0000

    initiated projects

---
+++ SRP_After/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRP_After
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ SRP_Before/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRP_Before
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
