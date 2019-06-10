bc3e3de5543eb5a5ffc8dbbcdf88ff76e8a755da
Randy <randy.l.mcbride@gmail.com>
2019-06-10 02:45:39 +0000

    started refactoring

---
+++ Branching_Before/Looping/After/PaintJobEstimator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OO_Patterns.Looping.After
+{
+	public static class PaintJobEstimator
+	{
+		public static Painter SelectCheapestPainter(double sqaureFeet, IEnumerable<Painter> painters) =>
+			painters.Where(painter => painter.IsAvailable).WithMinimum(painter => painter.GetEstimate(sqaureFeet));
+	}
+}
