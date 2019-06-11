3fa756ace3c20d9808205a9b418bfbdfb2d6400a
Randy <randy.l.mcbride@gmail.com>
2019-06-11 01:56:09 +0000

    Finished looping

---
+++ Branching_Before/Looping/After/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OO_Patterns.Looping.After
+{
+	public static class IEnumerableExtensions
+	{
+		public static T WithMinimum<T, Key>
+			(this IEnumerable<T> sequence, Func<T, Key> operation)
+			where Key : IComparable<Key> 
+			=> sequence
+				.Select(t => Tuple.Create(t, operation(t)))
+				.Aggregate((min, current) => current.Item2.CompareTo(min.Item2) < 0 ? current : min)
+				.Item1;
+	}
+}
---
+++ OO_Patterns_Tests/Looping/LoopingAfterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OO_Patterns.Looping;
+using OO_Patterns.Looping.After;
+using System.Collections.Generic;
+
+namespace OO_Patterns_Tests.Looping
+{
+	[TestClass]
+	public class LoopingAfterTests
+	{
+		[TestMethod]
+		public void GetsCheapestPainter()
+		{
+			var expensivePainter = new Painter { Rate = 100, IsAvailable = true };
+			var cheapPainter = new Painter { Rate = 50, IsAvailable = true };
+			var unavailablePainter = new Painter { Rate = 40, IsAvailable = false };
+			var painters = new List<Painter>
+			{
+				expensivePainter, cheapPainter, unavailablePainter
+			};
+			var cheapestAvailable = PaintJobEstimator.SelectCheapestPainter(100, painters);
+
+			Assert.AreEqual(cheapestAvailable, cheapPainter);
+		}
+	}
+}
