3a720ecbd3b7d319e31a3198386c2d0142850b2f
Randy <randy.mcbride@centrallogic.com>
2019-07-06 01:44:49 +0000

    started adding 'avoiding nulls' module

---
+++ Branching_Before/6 Avoiding Nulls/Before/SoldArticle.cs
@@ -0,0 +1,13 @@
+﻿namespace OO_Patterns._6_Avoiding_Nulls.Before
+{
+	public class SoldArticle
+	{
+		public Warranty MoneyBack { get; }
+		public Warranty Express { get; }
+		public SoldArticle(Warranty moneyBack, Warranty express)
+		{
+			MoneyBack = moneyBack;
+			Express = express;
+		}
+	}
+}
---
+++ Branching_Before/6 Avoiding Nulls/Before/Warranty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OO_Patterns._6_Avoiding_Nulls.Before
+{
+	public class Warranty
+	{
+		public DateTime IssueDate { get; }
+		public TimeSpan Duration { get; }
+		public Warranty(DateTime issueDate, TimeSpan duration)
+		{
+			IssueDate = issueDate;
+			Duration = duration;
+		}
+	}
+}
