4bd10aafb1697892025c1888464d07dfc383f019
Randy <randy.mcbride@centrallogic.com>
2019-09-04 22:10:23 +0000

    remove tips and traps

---
+++ CSharpTipsAndTraps/Tips/NullOrWhitespace.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTipsAndTraps.Tips
+{
+	[TestClass]
+	public class NullOrWhitespace
+	{
+		[TestMethod]
+		public void DetectsEmptyStrings()
+		{
+			Assert.IsTrue(string.IsNullOrWhiteSpace(string.Empty));
+		}
+	}
+}
